43e47598308648ba65d8c77f2c4ce50cd28b3ed7
joaoeduardodias <joaoeduardodias123@gmail.com>
2020-09-17 22:38:29 -0300

    Created listing  User and

--- backend/src/database/controllers/UserController.js
+++ backend/src/database/controllers/UserController.js
@@ -1,18 +1,35 @@
+const express = require('express')
+const crypto = require("crypto");
+const Connection = require("../../database/index")
 
-
-async function index(req, res) {
-
+async function index(res) {
+   const users = await Connection('Users').select('*')
+   return res.json(users)
 }
 
 async function create(req, res) {
-   const {
-        name,
-        email,
-        password,
-        whatsapp,
-        cpf, 
-        date_birth, 
-        administrador
-       } = req.body;
-       
-      }
+  const {
+    name,
+    email,
+    password,
+    whatsapp,
+    cpf,
+    date_birth,
+    administrador,
+  } = req.body;
+  const id = crypto.randomBytes(6).toString('HEX');
+
+ await Connection('Users').insert({
+   name,
+   email,
+   password,
+   whatsapp,
+   cpf,
+   date_birth,
+   administrador,
+     
+
+  })
+
+  return res.status(201).json({id})
+}
