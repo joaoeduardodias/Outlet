ead24f8fe3bb86be3308fb0830e76725454fa04c
Joao Dias <joaoeduardodias123@gmail.com>
2020-09-29 11:33:52 -0300

    Configure send email

--- backend/src/config/email/email.js
+++ backend/src/config/email/email.js
@@ -1,12 +1,26 @@
 const nodemailer = require('nodemailer')
+const { resolve } = require('path');
+const exphbs = require('express-handlebars');
+const hbs = require('nodemailer-express-handlebars')
 
 const transport = nodemailer.createTransport({
-   host: process.env.HOST,
-   port: process.env.PORT,
-   auth: {
-     user: process.env.USER,
-     pass: process.env.PASS
-   }
- });
+    host: process.env.HOST,
+    port: process.env.PORT,
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+    }
+});
+const viewPath = resolve(__dirname, '../../', 'resources', 'mail', 'auth');
+transport.use('compile', hbs({
+    viewEngine: exphbs.create({
+        layoutsDir: viewPath,
+        defaultLayout: 'forgotpassword',
 
- module.exports = transport;
+        extname: '.html',
+    }),
+    viewPath,
+    extName: '.html',
+}))
+
+module.exports = transport;
