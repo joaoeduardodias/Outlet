1cb410743bd1b0dc2356f30712f853a278928f3c
Joao Dias <joaoeduardodias123@gmail.com>
2020-09-30 15:46:08 -0300

    Integrate api

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,26 +1,40 @@
-async function getUsers() {
-    try {
-        const response = await fetch('http://localhost:3333/users')
-        const data = await response.json()
-        return data
-    } catch (error) {
-        console.error(error)
-    }
-}
-async function createUser() {
-    try {
-        // 			var myHeaders = new Headers();
-        // myHeaders.append("Content-Type", "text/plain");
-        // myHeaders.append("Content-Length", content.length.toString());
-        // myHeaders.append("X-Custom-Header", "ProcessThisImmediately")
-        const response = await fetch('http://localhost:3333/users', {
-            method: 'post',
-            body: new FormData(document.querySelector('#form-login'))
-        })
+const form = document.getElementById('form-login')
+form.onsubmit = function(e) {
+    login()
+    e.preventDefault()
 
+}
+const baseurl = 'http://localhost:3333'
+
+
+// async function getUsers() {
+//     try {
+//         const response = await fetch("http://localhost:3333/users");
+//         const data = await response.json();
+//         return data;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+async function login() {
+    try {
+        const { value: email } = document.getElementById("input-email");
+        const { value: password } = document.getElementById("input-password");
+
+
+        const headers = new Headers();
+        headers.append('Authorization', 'Basic ' + btoa(email + ":" + password))
+        headers.append('Content-Type', 'application/json')
+
+        const response = await fetch(baseurl + '/login', {
+            method: 'post',
+            headers: headers
+        })
         const data = await response.json()
-        return data
+        console.log(data)
     } catch (error) {
-        console.error(error)
+        console.log(errou)
+        console.error(error);
     }
 }
