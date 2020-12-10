015ddb4f2f9d6a6705963c834c42eda3c0190b83
Joao Dias <joaoeduardodias123@gmail.com>
2020-12-10 14:00:04 -0300

    add new migration

--- backend/src/database/migrations/20201210134941_Rename_column_id_image.js
+++ backend/src/database/migrations/20201210134941_Rename_column_id_image.js
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
-    return knex.schema.Table("Images", (table) => {
+    return knex.schema.table("Images", (table) => {
         table.renameColumn('id', 'id_image')
     })
 };
 
 exports.down = function(knex) {
-    return knex.schema.Table("Images", (table) => {
+    return knex.schema.table("Images", (table) => {
         table.renameColumn('id_image', 'id')
     })
 };
