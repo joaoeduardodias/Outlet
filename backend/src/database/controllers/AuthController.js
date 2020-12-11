a5dd24771449096237ef40cf92558d237ff66997
Joao Dias <joaoeduardodias123@gmail.com>
2020-12-11 11:06:39 -0300

    alter login

--- backend/src/database/controllers/AuthController.js
+++ backend/src/database/controllers/AuthController.js
@@ -12,7 +12,7 @@ module.exports = {
                 .toString()
                 .split(":");
 
-            const verifyUser = await Connection("Users").from('City')
+            const verifyUser = await Connection("Users")
                 .select("Users.email", "Users.password", "Users.id", "Users.name", "Users.administrador", "City.zip_code")
                 .where({ email })
                 .first();
