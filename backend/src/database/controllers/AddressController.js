b0b4722b88235987db847f48e962efe246123dd3
Joao Dias <joaoeduardodias123@gmail.com>
2020-12-11 09:01:15 -0300

    add state

--- backend/src/database/controllers/AddressController.js
+++ backend/src/database/controllers/AddressController.js
@@ -18,20 +18,20 @@ module.exports = {
             "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
         );
 
-        await Connection("Country").insert({
-            id: "1",
-            name: "Brasil"
-        })
+        // await Connection("Country").insert({
+        //     id: "1",
+        //     name: "Brasil"
+        // })
         data.map(async(item) => {
             let idState = item.id.toString();
 
-            // await Connection("State").insert({
-            //         id: idState,
-            //         name: item.nome,
-            //         id_country: "1",
-            //         uf: item.sigla,
-            //     })
-            // CIDADE
+            await Connection("State").insert({
+                    id: idState,
+                    name: item.nome,
+                    id_country: "1",
+                    uf: item.sigla,
+                })
+                // CIDADE
 
             // addcity(idState)
 
