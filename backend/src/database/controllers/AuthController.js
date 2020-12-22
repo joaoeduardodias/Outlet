8e8237d312d80707378af247ec6aa111c7b6bf23
Joao Dias <joaoeduardodias123@gmail.com>
2020-12-22 11:46:50 -0300

    bug fix

--- backend/src/database/controllers/AuthController.js
+++ backend/src/database/controllers/AuthController.js
@@ -34,7 +34,7 @@ module.exports = {
                     )
                     .where({ email })
                     .first();
-                User = verifyUser
+                return User = verifyUser
             }
 
             if (!User) {
