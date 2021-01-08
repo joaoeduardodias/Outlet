86286a583a790060226af7c08485e80e9b2e9aad
Joao Dias <joaoeduardodias123@gmail.com>
2021-01-08 14:20:54 -0300

    Payment of stripe

--- backend/src/database/controllers/PaymentController.js
+++ backend/src/database/controllers/PaymentController.js
@@ -1,38 +1,54 @@
 // SDK de Mercado Pago
-const mercadopago = require('mercadopago');
+// const mercadopago = require('mercadopago');
 module.exports = {
     async create(req, res) {
-        mercadopago.configurations.setAccessToken("TEST-3078541527341225-112412-e1d0f60d041f863c6d28410526c2913e-676623518");
+        //     mercadopago.configurations.setAccessToken("TEST-3078541527341225-112412-e1d0f60d041f863c6d28410526c2913e-676623518");
 
-        var payment_data = {
-            transaction_amount: Number(req.body.transaction_amount),
-            token: req.body.token,
-            description: req.body.description,
-            installments: Number(req.body.installments),
-            payment_method_id: req.body.paymentMethodId,
-            issuer_id: req.body.issuer,
-            payer: {
-                email: req.body.email,
-                identification: {
-                    type: req.body.docType,
-                    number: req.body.docNumber
-                }
-            }
-        };
+        //     var payment_data = {
+        //         transaction_amount: Number(req.body.transaction_amount),
+        //         token: req.body.token,
+        //         description: req.body.description,
+        //         installments: Number(req.body.installments),
+        //         payment_method_id: req.body.paymentMethodId,
+        //         issuer_id: req.body.issuer,
+        //         payer: {
+        //             email: req.body.email,
+        //             identification: {
+        //                 type: req.body.docType,
+        //                 number: req.body.docNumber
+        //             }
+        //         }
+        //     };
 
-        mercadopago.payment.save(payment_data)
-            .then(function(response) {
-                res.status(response.status).json({
-                    status: response.body.status,
-                    status_detail: response.body.status_detail,
-                    id: response.body.id
-                });
-            })
-            .catch(function(error) {
-                console.log(error)
-                res.status(response.status).send(error);
-            });
+        //     mercadopago.payment.save(payment_data)
+        //         .then(function(response) {
+        //             res.status(response.status).json({
+        //                 status: response.body.status,
+        //                 status_detail: response.body.status_detail,
+        //                 id: response.body.id
+        //             });
+        //         })
+        //         .catch(function(error) {
+        //             console.log(error)
+        //             res.status(response.status).send(error);
+        //         });
 
+        // }
+
+
+        //  STRIPE STRIPE STRIPE STRIPE STRIPE STRIPE 
+
+        const Stripe = require('stripe');
+        const stripe = Stripe(process.env.STRIPE_SECRETE_KEY);
+
+        // `source` is obtained with Stripe.js; see https://stripe.com/docs/payments/accept-a-payment-charges#web-create-token
+        const charge = await stripe.charges.create({
+            amount: 2000,
+            currency: 'brl',
+            source: 'tok_mastercard',
+            description: 'My First Test Charge (created for API docs)',
+        });
+
+        console.log(charge)
     }
-
 }
