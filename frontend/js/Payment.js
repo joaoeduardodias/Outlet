46d62e20640e8889e8feabf167a51d4ec5678e14
joaoeduardodias <joaoeduardodias123@gmail.com>
2021-01-10 18:18:29 -0300

    cart bugs resolv

--- frontend/js/Payment.js
+++ frontend/js/Payment.js
@@ -1,232 +1,146 @@
-window.Mercadopago.setPublishableKey("TEST-51f8db06-c035-4c78-a0d1-0e67fc404294");
-window.Mercadopago.getIdentificationTypes();
-const tokenUser = localStorage.getItem('Authorization')
-const zip_code = localStorage.getItem('Zip_code')
+// window.Mercadopago.setPublishableKey("TEST-51f8db06-c035-4c78-a0d1-0e67fc404294");
+// window.Mercadopago.getIdentificationTypes();
+
+var stripe = Stripe(
+  "pk_test_51I722hG3We8Vf5Y0ezP9rD5SKboSUgJRImKQ2LiBSpMCss1i7vSz4nKvCuietZYXmDcdC5qRS5jIN3DFkVmJRxeP00NZo4o97S"
+);
+
+const tokenUser = localStorage.getItem("Authorization");
+const zip_code = localStorage.getItem("Zip_code");
 const dataUser = JSON.parse(atob(tokenUser.split(".")[1]));
 
-document.getElementById('email').value = dataUser.email
-let Freight = {
-    weight,
-    zip_code,
-    lenght,
-    width,
-    height,
-    methodFreight: 'PAC'
-}
+document.getElementById("email").value = dataUser.email;
+let weight
+let width
+let height
+let lenght
+
+
+
+
 
 
 async function setPriceFreight() {
-    const DataFreight = await fetch(`${baseurl}/freight`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
+
+ const product = await fetch(`${baseurl}/Productshow/${idProduct}`,{
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+  },
+  mode: "cors",
+  })
+  const resProduct = await product.json()
+
+  let Freight = {
+    weight: resProduct.weight,
+    zip_code,
+    lenght: resProduct.lenght,
+    width: resProduct.width,
+    height: resProduct.height,
+    methodFreight: "PAC",
+  };
+
+
+  const DataFreight = await fetch(`${baseurl}/freight`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(Freight),
+  });
+
+  const DataFreightJson = await DataFreight.json();
+
+  const valorFreight = DataFreightJson.Valor.replace(",", ".");
+  const valorFreightFormat = parseFloat(valorFreight);
+  document.getElementById("price_freight").innerText = `Frete: ${valorFreight}`;
+  document.getElementById(
+    "date_freight"
+  ).innerText = `Prazo de entrega de ${DataFreightJson.PrazoEntrega} dias`;
+  const priceTotalFormat = parseFloat(priceTotal);
+  valueTotal = priceTotalFormat + valorFreightFormat;
+
+  cc(".purchase-modal .price-total").innerHTML = `Total R$: ${valueTotal.toFixed(2)}`;
+}
+
+// items que o usuario deseja comprar
+var purchase = {
+  items: {
+    id: idProduct,
+    price: valueTotal,
+  },
+};
+
+// desative o botao de pagamento até carregar o stripe
+document.querySelector("button").disabled = true;
+fetch(`${baseurl}/create-payment-intent`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(purchase),
+})
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (data) {
+    var elements = stripe.elements();
+
+    var style = {
+      base: {
+        color: "#32325d",
+        fontFamily: "Arial, sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d",
         },
-        mode: 'cors',
-        body: JSON.stringify(Freight)
+      },
+      invalid: {
+        fontFamily: "Arial, sans-serif",
+        color: "#fa755a",
+        iconColor: "#fa755a",
+      },
+    };
+
+    var card = elements.create("card", { style: style });
+    // Stripe injects an iframe into the DOM
+    card.mount("#card-element");
+
+    card.on("change", function (event) {
+      // Disable the Pay button if there are no card details in the Element
+      document.querySelector("button").disabled = event.empty;
+      document.querySelector("#card-error").textContent = event.error
+        ? event.error.message
+        : "";
+    });
+
+    var form = document.getElementById("payment-form");
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      // Complete payment when the submit button is clicked
+      payWithCard(stripe, card, data.clientSecret);
+    });
+  });
+
+var payWithCard = function (stripe, card, clientSecret) {
+  loading(true);
+  stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+      },
     })
-
-    const DataFreightJson = await DataFreight.json()
-
-
-
-
-    const valorFreight = DataFreightJson.Valor.replace(',', '.')
-    const valorFreightFormat = parseFloat(valorFreight)
-    document.getElementById('price_freight').innerText = `Valor do Frete: ${valorFreight}`
-    const priceTotalFormat = parseFloat(priceTotal)
-    valueTotal = priceTotalFormat + valorFreightFormat
-
-    cc(".purchase-modal .price-total").innerHTML = `Valor Total - R$: ${valueTotal.toFixed(2)}`
-}
-
-
-
-document.getElementById('cardNumber').addEventListener('change', guessPaymentMethod);
-
-function guessPaymentMethod(event) {
-    let cardnumber = document.getElementById("cardNumber").value;
-    if (cardnumber.length >= 6) {
-        let bin = cardnumber.substring(0, 6);
-        window.Mercadopago.getPaymentMethod({
-            "bin": bin
-        }, setPaymentMethod);
-    }
-};
-
-function setPaymentMethod(status, response) {
-    if (status == 200) {
-        let paymentMethod = response[0];
-        document.getElementById('paymentMethodId').value = paymentMethod.id;
-
-        getIssuers(paymentMethod.id);
-    } else {
-        alert(`payment method info error: ${response}`);
-    }
-}
-
-function getIssuers(paymentMethodId) {
-    window.Mercadopago.getIssuers(
-        paymentMethodId,
-        setIssuers
-    );
-}
-
-function setIssuers(status, response) {
-    if (status == 200) {
-        let issuerSelect = document.getElementById('issuer');
-        response.forEach(issuer => {
-            let opt = document.createElement('option');
-            opt.text = issuer.name;
-            opt.value = issuer.id;
-            issuerSelect.appendChild(opt);
-        });
-
-        getInstallments(
-            document.getElementById('paymentMethodId').value,
-            document.getElementById('transactionAmount').value = valueTotal,
-            issuerSelect.value
-        );
-    } else {
-        alert(`issuers method info error: ${response}`);
-    }
-}
-
-function getInstallments(paymentMethodId, transactionAmount, issuerId) {
-    window.Mercadopago.getInstallments({
-        "payment_method_id": paymentMethodId,
-        "amount": parseFloat(transactionAmount),
-        "issuer_id": parseInt(issuerId)
-    }, setInstallments);
-}
-
-function setInstallments(status, response) {
-    if (status == 200) {
-        document.getElementById('installments').options.length = 0;
-        response[0].payer_costs.forEach(payerCost => {
-            let opt = document.createElement('option');
-            opt.text = payerCost.recommended_message;
-            opt.value = payerCost.installments;
-            document.getElementById('installments').appendChild(opt);
-        });
-    } else {
-        alert(`installments method info error: ${response}`);
-    }
-}
-
-// gerando o token do cartao
-
-let doSubmit = false;
-document.getElementById('paymentForm').addEventListener('submit', getCardToken);
-
-function getCardToken(event) {
-    event.preventDefault();
-    if (!doSubmit) {
-        let $form = document.getElementById('paymentForm');
-        window.Mercadopago.createToken($form, setCardTokenAndPay);
-        return false;
-    }
-};
-
-async function setCardTokenAndPay(status, response) {
-    if (status == 200 || status == 201) {
-        let form = document.getElementById('paymentForm');
-        let card = document.createElement('input');
-        card.setAttribute('name', 'token');
-        card.setAttribute('type', 'hidden');
-        card.setAttribute('value', response.id);
-        form.appendChild(card);
-
-
-
-        const token = response.id
-        const description = document.getElementById('description').value
-        const installments = document.getElementById('installments').value
-        const payment_method_id = document.getElementById('paymentMethodId').value
-        const issuer_id = document.getElementById('issuer').value
-        const email = document.getElementById('email').value
-        const docType = document.getElementById('docType').value
-        const docNumber = document.getElementById('docNumber').value
-        const Payment = {
-            transaction_amount: valueTotal,
-            token,
-            description,
-            installments,
-            payment_method_id,
-            issuer_id,
-            email,
-            docType,
-            docNumber
-        }
-        const Response = await fetch(baseurl + '/process_payment', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-            mode: 'cors',
-            body: JSON.stringify(Payment)
-        })
-        const data = await Response.json()
-
-        const Product_Sold = {
-            value_total: valueTotal,
-            amount_sold: qtd,
-        }
-
-
-        switch (data.status_detail) {
-            case 'accredited':
-                alert(`Pronto, seu pagamento foi aprovado! `)
-                await fetch(`${baseurl}/product_sold/${idProduct}`, {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json",
-                        "Authorization": "Bearer " + tokenUser,
-                    },
-                    mode: 'cors',
-                    body: JSON.stringify(Product_Sold)
-
-                })
-                removeCart(Index)
-                break;
-            case 'pending_contingency':
-                alert(`Estamos processando o pagamento.
-                    Não se preocupe, em menos de 2 dias úteis
-                    informaremos por e-mail se foi creditado.`)
-                break;
-            case 'pending_review_manual':
-                alert(`Estamos processando seu pagamento.
-                    Não se preocupe, em menos de 2 dias úteis
-                    informaremos por e-mail se foi creditado ou se necessitamos de mais informação.`)
-                break;
-            case 'cc_rejected_bad_filled_card_number':
-                alert('Revise o número do cartão.')
-                break;
-            case 'cc_rejected_bad_filled_date':
-                alert('Revise a data de vencimento.')
-                break;
-            case 'cc_rejected_bad_filled_other':
-                alert('Revise os dados.')
-                break;
-            case 'cc_rejected_bad_filled_security_code':
-                alert('Revise o código de segurança do cartão.')
-                break;
-            case 'cc_rejected_card_error':
-                alert('Não conseguimos processar seu pagamento.')
-                break;
-            case 'cc_rejected_insufficient_amount':
-                alert('Saldo insuficiente')
-                break;
-            case 'cc_rejected_max_attempts':
-                alert('Você atingiu o limite de tentativas permitido.Escolha outro cartão ou outra forma de pagamento.')
-                break;
-
-        }
-
-
-    } else {
-        alert("Verify filled data!\n" + JSON.stringify(response, null, 4));
-    }
+    .then(function (result) {
+      if (result.error) {
+        // Show error to your customer
+        showError(result.error.message);
+      } else {
+        // The payment succeeded!
+        orderComplete(result.paymentIntent.id);
+      }
+    });
 };
--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -5,194 +5,188 @@ let priceTotal;
 let valueTotal;
 let qtd = 1;
 let Index;
-let idProduct;
-let weight;
-let typeWeight;
-let lenght;
-let width;
-let height;
+
+let idProduct
+
+let price;
 let screenWidth = screen.width;
 let cart = JSON.parse(localStorage.getItem("cart"));
 cart.map((item, index) => {
-    let price = item.price;
-    idProduct = item.id;
-    priceTotal = price;
-    weight = item.weight
-    typeWeight = item.typeWeight
-    lenght = item.lenght
-    width = item.width
-    height = item.height
 
-    let ProductItem = cc(".models .product").cloneNode(true);
+  let ProductItem = cc(".models .product").cloneNode(true);
 
-    ProductItem.querySelector(".product-info-img .product-img img").src =
-        item.images;
-    ProductItem.querySelector(".product-info-img .product-title h2").innerHTML =
-        item.title;
-    ProductItem.querySelector(
-        ".product-info-img .product-price"
-    ).innerHTML = `R$: ${item.price.toFixed(2)}`;
-    ProductItem.querySelector(
-        ".product--item--qtarea .product--item--qt"
-    ).innerHTML = qtd;
-    ProductItem.querySelector(
-        ".product-options .product-item-qtavailable"
-    ).innerHTML = `${item.amount} Disponíveis`;
-    ProductItem.querySelector(
-        ".product--item--qtarea .product--item-qtmenos"
-    ).addEventListener("click", () => {
-        if (qtd > 1) {
-            qtd--;
-            ProductItem.querySelector(
-                ".product--item--qtarea .product--item--qt"
-            ).innerHTML = qtd;
-            priceTotal = price * qtd;
-            ProductItem.querySelector(
-                ".product-info-img .product-price"
-            ).innerHTML = `R$: ${priceTotal.toFixed(2)}`;
+  ProductItem.querySelector(".product-info-img .product-img img").src =item.images;
+  ProductItem.querySelector(".product-info-img .product-title h2").innerHTML =item.title;
+  ProductItem.querySelector(".product-info-img .product-id").innerHTML =item.id;
+
+  ProductItem.querySelector(".product-info-img .product-price").innerHTML = `R$: ${item.price.toFixed(2)}`;
+  ProductItem.querySelector(".product--item--qtarea .product--item--qt").innerHTML = qtd;
+  ProductItem.querySelector(".product-options .product-item-qtavailable").innerHTML = `${item.amount} Disponíveis`;
+  ProductItem.querySelector(".product--item--qtarea .product--item-qtmenos").addEventListener("click", () => {
+    if (qtd > 1) {
+      qtd--;
+      ProductItem.querySelector(".product--item--qtarea .product--item--qt").innerHTML = qtd;
+      price = item.price;
+      priceTotal = price * qtd;
+      ProductItem.querySelector(".product-info-img .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
+    }
+  });
+
+  ProductItem.querySelector(".product--item--qtarea .product--item-qtmais").addEventListener("click", () => {
+    if (qtd < item.amount) {
+      qtd++;
+      ProductItem.querySelector(".product--item--qtarea .product--item--qt").innerHTML = qtd;
+      price = item.price;
+      priceTotal = price * qtd;
+      ProductItem.querySelector(".product-info-img .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
+    }
+  });
+
+  // excluir product do carrinho
+  ProductItem.querySelector(".product-options .product-trash").addEventListener("click",() => {
+      Index = index;
+      removeCart(Index);
+    }
+  );
+  //comprar
+  ProductItem.querySelector(".product-purchase").addEventListener("click",async () => {
+         Index = index;
+         idProduct = item.id
+        setPriceFreight();
+
+
+
+
+      cc(".windowpurchase").style.opacity = 0;
+      cc(".windowpurchase").style.display = "flex";
+      setTimeout(() => {
+        cc(".windowdetails").style.opacity = 0;
+        cc(".windowdetails").style.display = "none";
+        cc(".windowpurchase").style.opacity = 1;
+        cc(".purchase-modal .product-title h2").innerHTML = item.title;
+        price = item.price
+        priceTotal = price * qtd
+        cc(".purchase-modal .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
+        cc(".purchase-modal .product--item--qt").innerHTML = `Quantidade: ${qtd}`;
+        cc(".product-details .product-img img").src = item.images;
+      }, 200);
+      // close modal
+      const modal = cc(".purchase-modal");
+      cc(".windowpurchase").addEventListener("click", function (e) {
+        if (!modal.contains(e.target)) {
+          cc(".windowpurchase").style.opacity = 0;
+          qtd = 1;
+          ProductItem.querySelector(".product--item--qtarea .product--item--qt").innerHTML = qtd;
+          setTimeout(() => {
+            cc(".windowpurchase").style.display = "none";
+          }, 200);
         }
-    });
+      });
+    }
+  );
 
-    ProductItem.querySelector(
-        ".product--item--qtarea .product--item-qtmais"
-    ).addEventListener("click", () => {
-        if (qtd < item.amount) {
-            qtd++;
-            ProductItem.querySelector(".product--item--qtarea .product--item--qt").innerHTML = qtd;
-            priceTotal = price * qtd;
-            ProductItem.querySelector(
-                ".product-info-img .product-price"
-            ).innerHTML = `R$: ${priceTotal.toFixed(2)}`;
-        }
-    });
+  cc(".section-cart").append(ProductItem);
 
-    // excluir product do carrinho
-    ProductItem.querySelector(".product-options .product-trash").addEventListener(
+  if (screenWidth < 850) {
+    ProductItem.addEventListener("click", () => {
+      cc(".windowdetails").style.opacity = 0;
+      cc(".windowdetails").style.display = "flex";
+      setTimeout(() => {
+        cc(".windowdetails").style.opacity = 1;
+        cc("#body-modal").style.overflow = "hidden";
+
+        cc(".product-modal .product-img #img").src = item.images;
+        cc(".product-modal .product-title h2").innerHTML = item.title;
+        cc(".product-modal .product-price").innerHTML = `R$: ${item.price.toFixed(2)}`;
+      }, 200);
+      cc(".product-modal .product--item-qtmenos").addEventListener(
         "click",
         () => {
-
-            Index = index
-            removeCart(Index)
+          if (qtd > 1) {
+            qtd--;
+            cc(".product-modal .product--item--qt").innerHTML = qtd;
+            price = item.price;
+            priceTotal = price * qtd;
+            cc(".product-modal .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
+          }
         }
-    );
-    //comprar
-    ProductItem.querySelector(".product-purchase")
-        .addEventListener("click", async() => {
-            Index = index
-            setPriceFreight()
+      );
+      cc(".product-modal .product--item-qtmais").addEventListener(
+        "click",
+        () => {
+          if (qtd < item.amount) {
+            qtd++;
+            cc(".product-modal .product--item--qt").innerHTML = qtd;
+            price = item.price;
+            priceTotal = price * qtd;
+            cc(".product-modal .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
+          }
+        }
+      );
+      // comprar
+      cc(".product-modal .product-purchase").addEventListener("click", () => {
+        Index = index;
+        idProduct = item.id
 
+        setPriceFreight();
+
+        cc(".windowpurchase").style.opacity = 0;
+        cc(".windowpurchase").style.display = "flex";
+        setTimeout(() => {
+          cc(".windowdetails").style.opacity = 0;
+          cc(".windowdetails").style.display = "none";
+          cc(".windowpurchase").style.opacity = 1;
+          cc(".purchase-modal .product-title h2").innerHTML = item.title;
+          price = item.price;
+            priceTotal = price * qtd;
+          cc(".purchase-modal .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
+          cc(".purchase-modal .product--item--qt").innerHTML = `Quantidade: ${qtd}`;
+          cc(".product-details .product-img img").src = item.images;
+        }, 200);
+        // close modal
+        const modal = cc(".purchase-modal");
+        cc(".windowpurchase").addEventListener("click", function (e) {
+          if (!modal.contains(e.target)) {
             cc(".windowpurchase").style.opacity = 0;
-            cc(".windowpurchase").style.display = "flex";
+            qtd = 1;
+            cc(".product-modal .product--item--qt").innerHTML = qtd;
             setTimeout(() => {
-                cc(".windowpurchase").style.opacity = 1;
-                cc(".purchase-modal .product-title h2").innerHTML = item.title;
-                cc(".purchase-modal .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
-                cc(".purchase-modal .product--item--qt").innerHTML = `Quantidade : ${qtd}`;
-
-
-
+              cc(".windowpurchase").style.display = "none";
             }, 200);
-            // close modal
-            const modal = cc(".purchase-modal");
-            cc(".windowpurchase").addEventListener("click", function(e) {
-                if (!modal.contains(e.target)) {
-                    cc(".windowpurchase").style.opacity = 0;
-                    setTimeout(() => {
-                        cc(".windowpurchase").style.display = "none";
-
-                    }, 200);
-                }
-            });
+          }
         });
+      });
 
-    cc(".section-cart").append(ProductItem);
-
-    if (screenWidth < 850) {
-        ProductItem.addEventListener("click", () => {
-            cc(".windowdetails").style.opacity = 0;
-            cc(".windowdetails").style.display = "flex";
-            setTimeout(() => {
-                cc(".windowdetails").style.opacity = 1;
-                cc("#body-modal").style.overflow = "hidden";
-
-                cc(".product-modal .product-img #img").src = item.images;
-                cc(".product-modal .product-title h2").innerHTML = item.title;
-                cc(".product-modal .product-price").innerHTML = `R$: ${item.price.toFixed(2)}`;
-            }, 200);
-            cc(".product-modal .product--item-qtmenos").addEventListener("click", () => {
-                if (qtd > 1) {
-                    qtd--;
-                    cc(".product-modal .product--item--qt").innerHTML = qtd;
-                    priceTotal = price * qtd;
-                    cc(".product-modal .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
-                }
-            });
-            cc(".product-modal .product--item-qtmais").addEventListener("click", () => {
-                if (qtd < item.amount) {
-                    qtd++;
-                    cc(".product-modal .product--item--qt").innerHTML = qtd;
-                    priceTotal = price * qtd;
-                    cc(".product-modal .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
-                }
-            });
-            cc(".product-modal .product-purchase").addEventListener("click", () => {
-                Index = index
-                setPriceFreight()
-
-                cc(".windowpurchase").style.opacity = 0;
-                cc(".windowpurchase").style.display = "flex";
-                setTimeout(() => {
-                    cc(".windowpurchase").style.opacity = 1;
-                    cc(".purchase-modal .product-title h2").innerHTML = item.title;
-                    cc(".purchase-modal .product-price").innerHTML = `R$: ${priceTotal.toFixed(2)}`;
-                    cc(".purchase-modal .product--item--qt").innerHTML = `Quantidade : ${qtd}`;
-
-
-
-                }, 200);
-                // close modal
-                const modal = cc(".purchase-modal");
-                cc(".windowpurchase").addEventListener("click", function(e) {
-                    if (!modal.contains(e.target)) {
-                        cc(".windowpurchase").style.opacity = 0;
-                        setTimeout(() => {
-                            cc(".windowpurchase").style.display = "none";
-
-                        }, 200);
-                    }
-                });
-            })
-
-            // close modal
-            const modal = cc(".product-modal");
-            cc(".windowdetails").addEventListener("click", function(e) {
-                if (!modal.contains(e.target)) {
-                    cc(".windowdetails").style.opacity = 0;
-                    setTimeout(() => {
-                        cc(".windowdetails").style.display = "none";
-                        cc("#body-modal").style.overflow = "initial";
-                        qtd = 1;
-                        cc(".product-modal .product--item--qt").innerHTML = qtd;
-                    }, 200);
-                }
-            });
-            // excluir product do carrinho
-            cc(".product-modal .product-trash").addEventListener("click", () => {
-                cc(".product").style.opacity = 1;
-                setTimeout(() => {
-                    cc(".product").style.opacity = 0;
-                    cc(".product").style.display = "none";
-                }, 200);
-                cart.splice(index, 1);
-                localStorage.setItem("cart", JSON.stringify(cart));
-                location.reload();
-            });
-        });
-    }
+      // close modal
+      const modal = cc(".product-modal");
+      cc(".windowdetails").addEventListener("click", function (e) {
+        if (!modal.contains(e.target)) {
+          cc(".windowdetails").style.opacity = 0;
+          setTimeout(() => {
+            cc(".windowdetails").style.display = "none";
+            cc("#body-modal").style.overflow = "initial";
+            qtd = 1;
+            cc(".product-modal .product--item--qt").innerHTML = qtd;
+          }, 200);
+        }
+      });
+      // excluir product do carrinho
+      cc(".product-modal .product-trash").addEventListener("click", () => {
+        cc(".product").style.opacity = 1;
+        setTimeout(() => {
+          cc(".product").style.opacity = 0;
+          cc(".product").style.display = "none";
+        }, 200);
+        cart.splice(index, 1);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        location.reload();
+      });
+    });
+  }
 });
 
 function removeCart(Index) {
-    cart.splice(Index, 1);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    location.reload();
+  cart.splice(Index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  location.reload();
 }
