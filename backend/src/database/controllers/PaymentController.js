00e0a86743fa4c1c65d46fc010e5925cf68b7c06
Joao Dias <joaoeduardodias123@gmail.com>
2021-01-11 10:54:41 -0300

    create calculateOrderAmount stripe

--- backend/src/database/controllers/PaymentController.js
+++ backend/src/database/controllers/PaymentController.js
@@ -4,9 +4,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRETE_KEY)
 const calculateOrderAmount = (items) => {
     // calcular o preço total aqui
     const price = parseFloat(items.price)
-    const total = price.replace('.', ',')
+        // const total = price.replace('.', ',')
         // const priceincents = total * 100
-    console.log(total)
+    console.log(typeof price)
         // console.log(priceincents)
     return 1400; // valor do produto
 };
