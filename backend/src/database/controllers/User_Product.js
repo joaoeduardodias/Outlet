f7641f06d55d6003f2b62549864e6779f0451e1e
Joao Dias <joaoeduardodias123@gmail.com>
2021-01-27 09:01:04 -0300

    relatory of solds

--- backend/src/database/controllers/User_Product.js
+++ backend/src/database/controllers/User_Product.js
@@ -12,6 +12,7 @@ module.exports = {
                     "id_user",
                     "amount_sold",
                     "value_total",
+                    "tracking",
                     "Products.name"
                 );
 
