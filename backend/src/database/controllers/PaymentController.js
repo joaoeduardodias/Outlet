70a885135d8c7cd142328abb5a0c602a0521969c
Joao Dias <joaoeduardodias123@gmail.com>
2021-02-03 14:38:44 -0300

    create a payment of paypal

--- backend/src/database/controllers/PaymentController.js
+++ backend/src/database/controllers/PaymentController.js
@@ -1,12 +1,13 @@
 const paypal = require('paypal-rest-sdk')
-const paypalConfig = require('../../config/payment.json')
-paypal.configure(paypalConfig)
+paypal.configure(PROCCESS.ENV.PAY_MODE, PROCCESS.ENV.PAY_CLIENT_ID, PROCCESS.ENV.PAY_CLIENT_SECRET)
 
 
 module.exports = {
     // quando clicar para pagar 
     async create(req, res) {
-        res.send({ success: true })
+        const { price, id: product_id, amount_sold: quantity } = req.body
+        console.log(price, product_id, quantity)
+
     },
     // quando pagar com sucesso
     async success(req, res) {
