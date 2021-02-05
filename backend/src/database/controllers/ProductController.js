75e3c3bfeffff4be8c4ced69b4c92e62efdec44c
Joao Dias <joaoeduardodias123@gmail.com>
2021-02-05 15:30:15 -0300

    CRUD of attributes of products

--- backend/src/database/controllers/ProductController.js
+++ backend/src/database/controllers/ProductController.js
@@ -169,10 +169,10 @@ module.exports = {
                     return res.status(201).json({ message: 'create', id });
 
                 })
-                .catch(function(error) {
-                    return res.status(500).json({ message: 'error', error });
+                // .catch(function(error) {
+                //     return res.status(500).json({ message: 'error', error });
 
-                });
+            // });
 
         } catch (error) {
             // next(error)
