6173a601c39dee5db1921db9cbeb27524987b966
joaoeduardodias <joaoeduardodias123@gmail.com>
2021-03-07 10:39:02 -0300

    bug fix

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,50 +1,32 @@
 // Update with your config settings.
 
 module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./src/database/db.sqlite",
+    },
+    migrations: {
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    useNullAsDefault: true,
+  },
 
-    development: {
-        client: 'sqlite3',
-        connection: {
-            filename: './src/database/db.sqlite'
-        },
-        migrations: {
-            directory: `${__dirname}/src/database/migrations`
-        },
-        useNullAsDefault: true
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: `${__dirname}/src/database/migrations`,
     },
 
-    staging: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: `${__dirname}/src/database/migrations`
-        },
+    ssl: {
+      rejectUnauthorized: true,
     },
+    useNullAsDefault: true,
 
-    production: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: `${__dirname}/src/database/migrations`
-        },
-
-        ssl: {
-            rejectUnauthorized: false
-        },
-        useNullAsDefault: true,
-
-        pool: {
-            min: 2,
-            max: 10
-        }
-    }
-
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
 };
