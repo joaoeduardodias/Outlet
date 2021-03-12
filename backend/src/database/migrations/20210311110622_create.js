dfb50de73e537332d2fa6901af63053b4966dca3
joaoeduardodias <joaoeduardodias123@gmail.com>
2021-03-12 15:55:33 -0300

    create a order

--- backend/src/database/migrations/20210311110622_create.js
+++ backend/src/database/migrations/20210311110622_create.js
@@ -1,19 +1,19 @@
-exports.up = function (knex) {
-  return knex.schema.createTable("Orders", (table) => {
-    table.string("id_order").primary().notNullable();
-    table.jsonb("ids_sold");
-    table
-      .foreign("id_user")
-      .references("id")
-      .inTable("Users")
-      .onDelete("CASCADE")
-      .onUpdate("CASCADE");
+// exports.up = function (knex) {
+//   return knex.schema.createTable("Orders", (table) => {
+//     table.string("id_order").primary().notNullable();
+//     table.jsonb("ids_sold");
+//     table
+//       .foreign("id_user")
+//       .references("id")
+//       .inTable("Users")
+//       .onDelete("CASCADE")
+//       .onUpdate("CASCADE");
 
-    table.timestamp("created_at").defaultTo(knex.fn.now());
-    table.timestamp("updated_at").defaultTo(knex.fn.now());
-  });
-};
+//     table.timestamp("created_at").defaultTo(knex.fn.now());
+//     table.timestamp("updated_at").defaultTo(knex.fn.now());
+//   });
+// };
 
-exports.down = function (knex) {
-  return knex.schema.dropTable("Orders");
-};
+// exports.down = function (knex) {
+//   return knex.schema.dropTable("Orders");
+// };
