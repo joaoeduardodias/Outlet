e9043c29376bdf5bc96548437c60b332d23f9dc1
joaoeduardodias <joaoeduardodias123@gmail.com>
2021-03-20 14:51:36 -0300

    send email

--- backend/src/config/email/email.js
+++ backend/src/config/email/email.js
@@ -2,7 +2,6 @@ const nodemailer = require("nodemailer");
 const viewPath = resolve(__dirname, "../../", "resources", "mail");
 const exphbs = require("express-handlebars");
 const hbs = require("nodemailer-express-handlebars");
-const transport = require("../../config/email/email");
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
